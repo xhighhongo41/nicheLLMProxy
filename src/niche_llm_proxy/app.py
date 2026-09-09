5baacb041dad18b456958d3af7d854f5564624d7
xhighhongo41 <xhigh.hongo.41@gmail.com>
2026-09-10 07:52:48 +0900

    chore: bump version to 1.2.1

--- src/niche_llm_proxy/app.py
+++ src/niche_llm_proxy/app.py
@@ -68,7 +68,7 @@ def create_app(
 
     app = FastAPI(
         title="nicheLLM Proxy",
-        version="1.2.0",
+        version="1.2.1",
         docs_url=None,
         redoc_url=None,
         lifespan=lifespan,
